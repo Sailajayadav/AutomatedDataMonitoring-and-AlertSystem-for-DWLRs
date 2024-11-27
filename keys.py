f7a4ca05cef5576bc45dcd68038b1fd94e48b2bc
Sailajayadav <veerlasailajayadav@gmail.com>
2024-11-27 20:47:06 +0530

    Initial commit

---
+++ keys.py
@@ -0,0 +1,8 @@
+account_sid = 'AC3303e4a06c4efa33dd38020742fadf6a'
+auth_token = '3b09d5436bb6a0fa54d2794031168027'
+
+twilio_number = '+18144028346'
+target_number = '+919030942770'
+
+
+rec = 'TEYSMJYXM7HV7269SGFW9RVV'
